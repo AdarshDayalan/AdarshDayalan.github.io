33aa1883e43860c047498db382582439a7d92453
AdarshDayalan <adarshdayal7@gmail.com>
2022-01-13 16:46:24 -0600

    Fixed blank background pop-up

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const img_id = init_label(9)
-const img_xy = init_xy(img_id)
+const img_id = init_label(9);
+const img_xy = init_xy(img_id);
 
-const wiggle_mult = 1.0
+const wiggle_mult = 1.0;
 
-const mid_x = screen.width/2
-const mid_y = screen.height/2
+const mid_x = screen.width/2;
+const mid_y = screen.height/2;
 
-let pop = false
+let pop = false;
 
 function vh(v) {
     var h = document.documentElement.clientHeight;
@@ -24,22 +24,22 @@ function coordinate(event) {
 
     if(screen.width > 800) {
         for (const [idx, _] of img_id.entries()) {
-            let r = idx % 4
+            let r = idx % 4;
             if(r == 0) {
                 move4(x,y,img_id[idx], img_xy[idx])
-            } else if (r == 1){
-                move3(x,y,img_id[idx], img_xy[idx])
+            } else if (r == 1){;
+                move3(x,y,img_id[idx], img_xy[idx]);
             } else if (r == 2){
-                move2(x,y,img_id[idx], img_xy[idx])
+                move2(x,y,img_id[idx], img_xy[idx]);
             } else {
-                move1(x,y,img_id[idx], img_xy[idx])
+                move1(x,y,img_id[idx], img_xy[idx]);
             }
         }
     }
  }
 
 function show_pop_up() {
-    pop = true
+    pop = true;
 
     const landing_content = document.getElementById("center");
     landing_content.style.visibility = "hidden";
@@ -55,7 +55,7 @@ function show_pop_up() {
 
     document.body.style.overflowY = "hidden";
 
-    landing_content.ontransitionend = () => {
+    document.body.ontransitionend = () => {
         if(pop) {
             const pop_up = document.getElementById("pop-up");
             pop_up.style.visibility = "visible";
@@ -66,7 +66,7 @@ function show_pop_up() {
 
 function reset() {
 
-    pop = false
+    pop = false;
     
     const pop_up = document.getElementById("pop-up");
     pop_up.style.visibility = "hidden";
@@ -93,44 +93,43 @@ function reset() {
 }
 
  function move1(x, y, img, img_xy) {
-    let [img_x, img_y] = img_xy
-    img.style.left = img_x - vw(((x-mid_x)/mid_x)  * wiggle_mult * img.width) + "vw"
-    img.style.top = img_y - vh(((y-mid_y)/mid_y) * wiggle_mult * img.height) + "vh"
+    let [img_x, img_y] = img_xy;
+    img.style.left = img_x - vw(((x-mid_x)/mid_x)  * wiggle_mult * img.width) + "vw";
+    img.style.top = img_y - vh(((y-mid_y)/mid_y) * wiggle_mult * img.height) + "vh";
  }
 
  function move2(x, y, img, img_xy) {
-    let [img_x, img_y] = img_xy
-    img.style.left = img_x + vw(((x-mid_x)/mid_x)  * wiggle_mult * img.width) + "vw"
-    img.style.top = img_y + vh(((y-mid_y)/mid_y) * wiggle_mult * img.height) + "vh"
+    let [img_x, img_y] = img_xy;
+    img.style.left = img_x + vw(((x-mid_x)/mid_x)  * wiggle_mult * img.width) + "vw";
+    img.style.top = img_y + vh(((y-mid_y)/mid_y) * wiggle_mult * img.height) + "vh";
  }
 
  function move3(x, y, img, img_xy) {
-    let [img_x, img_y] = img_xy
-    img.style.left = img_x + vw(((x-mid_x)/mid_x) * wiggle_mult * img.width) + "vw"
-    img.style.top = img_y - vh(((y-mid_y)/mid_y) * wiggle_mult * img.height) + "vh"
+    let [img_x, img_y] = img_xy;
+    img.style.left = img_x + vw(((x-mid_x)/mid_x) * wiggle_mult * img.width) + "vw";
+    img.style.top = img_y - vh(((y-mid_y)/mid_y) * wiggle_mult * img.height) + "vh";
  }
 
  function move4(x, y, img, img_xy) {
-    let [img_x, img_y] = img_xy
-    img.style.left = img_x - vw(((x-mid_x)/mid_x)  * img.width) + "vw"
-    img.style.top = img_y + vh(((y-mid_y)/mid_y) * img.height) + "vh"
+    let [img_x, img_y] = img_xy;
+    img.style.left = img_x - vw(((x-mid_x)/mid_x)  * img.width) + "vw";
+    img.style.top = img_y + vh(((y-mid_y)/mid_y) * img.height) + "vh";
  }
 
  function init_label(n) {
-     const arr = []
+     const arr = [];
      for (let step = 0; step < n; step++) {
-         arr.push(document.getElementById(("img"+step)))
+         arr.push(document.getElementById(("img"+step)));
      }
-     return arr
+     return arr;
  }
 
  function init_xy(arr) {
-     arr_xy = []
+     arr_xy = [];
     for (const img of arr) {
-        arr_xy.push([vw(img.offsetLeft), vh(img.offsetTop)])
+        arr_xy.push([vw(img.offsetLeft), vh(img.offsetTop)]);
     }
-    // console.log(arr_xy[0])
-    return arr_xy
+    return arr_xy;
 }
 
 function ToggleDark() {
@@ -140,16 +139,15 @@ function ToggleDark() {
     const nav_logo = document.getElementById("nav-logo")
     const footer_logo = document.getElementById("footer-img")
 
-    console.log(btn.src)
     if(btn.src.includes("png%20images/btn-grey.png")) {
         btn.src = "png images/btn-white.png";
-        arw.src = "png images/arrow-down-white.png"
-        nav_logo.src = "Images/logo_crop_white.png"
-        footer_logo.src = "Images/logo_crop_grey.png"
+        arw.src = "png images/arrow-down-white.png";
+        nav_logo.src = "Images/logo_crop_white.png";
+        footer_logo.src = "Images/logo_crop_grey.png";
     } else {
         btn.src = "png images/btn-grey.png";
-        arw.src = "png images/arrow-down-grey.png"
-        nav_logo.src = "Images/logo_crop_grey.png"
-        footer_logo.src = "Images/logo_crop_white.png"
+        arw.src = "png images/arrow-down-grey.png";
+        nav_logo.src = "Images/logo_crop_grey.png";
+        footer_logo.src = "Images/logo_crop_white.png";
     }
   }
